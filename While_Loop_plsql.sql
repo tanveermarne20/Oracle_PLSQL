487178cab9d026202b37873c629d66ffb16da2db
tanveermarne20 <>
2024-03-05 19:23:41 +0530

    new added

--- While_Loop_plsql.sql
+++ While_Loop_plsql.sql
@@ -110,37 +110,42 @@ BEGIN
      END IF;
      NUM:=NUM+1;
    END LOOP;
-END;
+END; 
 
---5.	Write a Plsql program to print all odd number between 1 to 100.
-SET SERVEROUTPUT ON
-DECLARE
- F NUMBER:=1;
-BEGIN
- WHILE (F<=100) LOOP
-    IF MOD(F,2)=1 THEN
-     DBMS_OUTPUT.PUT_LINE(F);
-   END IF;
-   F:=F+1;
- END LOOP; 
-END;
-
---6.	Write a Plsql program to find sum of all natural numbers between 1 to n.
+--6.Write a Plsql program to find sum of all natural numbers between 1 to n.
 --Natural numbers are a set of positive integers starting from 1 and continuing indefinitely
 SET SERVEROUTPUT ON
 DECLARE
-   N NUMBER := &NO1;
-   J NUMBER := 1;
-   SUM NUMBER := 0;
+  NO1 NUMBER := &NO1;
+  SUM1 NUMBER := 0;
+  I NUMBER := 1;
 BEGIN
-   WHILE (J <= N) LOOP
-      DBMS_OUTPUT.PUT_LINE(J);
-      SUM := SUM + J;
-      J := J + 1;
-   END LOOP;
-   DBMS_OUTPUT.PUT_LINE('Sum of natural numbers from 1 to ' || N || ' is: ' || SUM);
+  WHILE I <= NO1 LOOP            --1<=30 ,2<=30
+    SUM1 := SUM1 + I;             --0+1+2
+    I := I + 1;                     --1+1=2, 2+1
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE(SUM1);
 END;
+
 --7.	Write a Plsql program to find sum of all even numbers between 1 to n.
+SET SERVEROUTPUT ON
+DECLARE
+NUM1 NUMBER:=&NUM1;
+U NUMBER:=1;
+SUM1 NUMBER :=0;
+BEGIN
+   WHILE (U<=NUM1)  LOOP 
+      IF MOD(U,2)=0 THEN    
+          SUM1:=SUM1+U;
+          END IF;
+           U:=U+1;
+   END LOOP;
+   DBMS_OUTPUT.PUT_LINE(SUM1);
+END;
+
+
+
+
 --8.	Write a Plsql program to find sum of all odd numbers between 1 to n.
 --9.	Write a Plsql program to print multiplication table of any number.
 --10.	Write a Plsql program to count number of digits in a number.
@@ -166,8 +171,164 @@ END;
 --30.	Write a plsql program to print all Perfect numbers between 1 to n.
 --31.	Write a plsql program to check whether a number is Strong number or not.
 --32.	Write a plsql program to print all Strong numbers between 1 to n.
+SET SERVEROUTPUT ON
+
+DECLARE
+   N NUMBER := &NO1; -- Enter the value of N
+   i NUMBER;
+   num NUMBER;
+   digit NUMBER;
+   factorial NUMBER;
+   sum_factorials NUMBER;
+   
+   FUNCTION factorial_func(num NUMBER) RETURN NUMBER IS
+      result NUMBER := 1;
+   BEGIN
+      FOR i IN 1..num LOOP
+         result := result * i;
+      END LOOP;
+      RETURN result;
+   END factorial_func;
+
+BEGIN
+   DBMS_OUTPUT.PUT_LINE('Strong numbers between 1 and ' || N || ':');
+
+   FOR num IN 1..N LOOP
+      i := num;
+      sum_factorials := 0;
+
+      WHILE i > 0 LOOP
+         digit := i MOD 10;
+         factorial := factorial_func(digit);
+         sum_factorials := sum_factorials + factorial;
+         i := TRUNC(i / 10);
+      END LOOP;
+
+      IF num = sum_factorials THEN
+         DBMS_OUTPUT.PUT_LINE(num);
+      END IF;
+   END LOOP;
+END;
+
 --33.	Write a plsql program to print Fibonacci series up to n terms.
+
 --34.	Write a plsql program to print Pascal triangle upto n rows.
+
+-------------------------******************---------------------------------------
+
+
+--PRINT ALL NATURAL NOS FROM 1 TO N
+SET SERVEROUTPUT ON
+DECLARE
+N NUMBER:=&NTH;
+P NUMBER:=1;
+BEGIN
+   WHILE (P<=N)  LOOP                   --1<=20
+         DBMS_OUTPUT.PUT_LINE(P);           --1
+         P:=P+1;                               --1+1=2
+   END LOOP;
+END;
+
+
+--PRINT TABLE OF 23
+SET SERVEROUTPUT ON
+DECLARE
+ NUM1 NUMBER:=&NUM1;
+ P NUMBER:=1;
+ RSLT NUMBER;
+BEGIN
+     LOOP
+          RSLT:=NUM1*P;                ---25*1    
+          DBMS_OUTPUT.PUT_LINE(RSLT);        --25
+          P:=P+1;
+          EXIT WHEN P=11;           
+     END LOOP;
+END;
+
+-----------------------------------------------------
+SET SERVEROUTPUT ON
+DECLARE
+ NUM1 NUMBER:=&NUM1; 
+I NUMBER:=1;
+RSLT NUMBER;
+BEGIN
+   WHILE (I<11)  LOOP
+      RSLT:=NUM1*I;
+      DBMS_OUTPUT.PUT_LINE(RSLT);
+      I:=I+1;
+   END LOOP;
+END;
+
+--PRINT NUMBER OF 89 USING WHILE LOOP
+
+SET SERVEROUTPUT ON
+DECLARE
+ NUM1 NUMBER:=&NO1;
+ P NUMBER :=1;
+ RSLT NUMBER;
+BEGIN
+   WHILE (P<11)  LOOP
+     RSLT:=NUM1*P;
+     DBMS_OUTPUT.PUT_LINE(RSLT);
+      P:=P+1; 
+   END LOOP;
+END;
+
+
+
+SET SERVEROUTPUT ON
+DECLARE
+ NUM1 NUMBER:=&NUM1;
+ P NUMBER:=1;
+ RSLT NUMBER;
+BEGIN
+   LOOP
+         RSLT:=NUM1*P;
+          DBMS_OUTPUT.PUT_LINE(RSLT);
+            P:=P+1;
+            EXIT WHEN P=11;
+   END LOOP;
+END;
+
+----------------------------------------------------------------
+--PRINT TABLE OF 96 USING BASIC LOOP
+
+SET SERVEROUTPUT ON
+DECLARE
+NUM1 NUMBER:=&NO1;
+M NUMBER:=1;
+RSLT NUMBER;
+BEGIN
+   LOOP
+        RSLT:=NUM1*M;                    --96*1;
+        DBMS_OUTPUT.PUT_LINE(RSLT);             --96
+         M:=M+1;                                  --1+1=2
+         EXIT WHEN M=11;
+   END LOOP;
+END;
+
+
+
+
+
+
+
+-----------------------------------------------------------------------
+--PRINT NATURAL NUMBER FROM 1 TO N
+SET SERVEROUTPUT ON
+DECLARE
+N NUMBER:=&NTH;
+K NUMBER:=1;
+BEGIN
+     WHILE (K<=N)  LOOP
+             DBMS_OUTPUT.PUT_LINE(K);
+     END LOOP;
+       
+END;
+
+
+
+
 
 
 
